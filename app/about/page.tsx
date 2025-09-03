17f585951226f4400c4e3dcb91f22f571f30775d
henlam <henry.lam.1@stonybrook.edu>
2025-09-02 23:11:57 -0400

    Added about page information

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,12 @@
 export default function AboutPage() {
-  return <h1>About Page</h1>;
+  return (
+    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+      <h1 className="text-4xl font-bold mb-8">About Page</h1>
+      <p>
+        This CMS-powered blog app was meant to help me learn the Next.js
+        framework and content management systems. I decided to choose Sanity
+        because it's a headless CMS and it offers schemas to structure content.
+      </p>
+    </main>
+  );
 }
